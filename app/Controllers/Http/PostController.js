94e9ac755eb046ca5647dfd82983a94639c5613f
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-09-13 14:17:16 +0700

    create controller, view, and routes post

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,25 @@
+'use strict'
+//load model Post
+const Post = use('App/Models/Post')
+
+class PostController {
+
+  async index({ view }){
+    const posts = await Post.all();
+
+    return view.render('posts.index',{
+      title: 'Latest Post',
+      posts: posts.toJSON()
+    })
+  }
+
+  async details({params, view}){
+    const post = await Post.find(params.id)
+
+    return view.render('posts.details',{
+      post: post
+    })
+  }
+}
+
+module.exports = PostController
