e89e786cd9ad9c6176da7edddaebb34e1989bb01
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-09-14 10:19:42 +0700

    save data from add post

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -24,6 +24,18 @@ class PostController {
   async add({view}){
     return view.render('posts.add')
   }
+
+  async store({ request, response, session }){
+    const post = new Post()
+
+    post.title = request.input('title')
+    post.body = request.input('body')
+
+    await post.save()
+
+    session.flash({ notification: 'Post Added!'})
+    return response.redirect('/posts')
+  }
 }
 
 module.exports = PostController
